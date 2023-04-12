c3b88a85740fb0cbd56872495a36bec6de6b9e6c
latinyloco <a.castrol.2020@alumnos.urjc.es>
2023-04-12 10:30:33 +0200

    UnitTest hechos
    Comprobar que funcionan.

---
+++ app/src/test/java/com/example/ps_g8/UnitTest.java
@@ -0,0 +1,60 @@
+package com.example.ps_g8;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/*public String getTitulo() {
+        return titulo;
+}*/
+
+/*public void setTitulo(String titulo) {
+        this.titulo = titulo;
+}*/
+
+/*public String getDescripcion() {
+        return descripcion;
+}*/
+
+/*public String getSinopsis() {
+        return sinopsis;
+}*/
+
+public class UnitTest {
+    @Test
+    public void testgetTitulo() {
+        Pelicula peli = new Pelicula(1, 1, "Esto es un título", "Esto es una descripción", "Esto es una sinopsis", 0, 0);
+        String tit = "Esto es un título";
+        result = peli.getTitulo();
+        assertEquals(tit, result);
+    }
+
+
+    @Test
+    public void testsetTitulo() {
+        Pelicula peli = new Pelicula(1, 1, "Esto es un título", "Esto es una descripción", "Esto es una sinopsis", 0, 0);
+        String tit = "Labella y la bestia";
+        peli.setTitulo(tit);
+        result = peli.getTitulo();
+        assertEquals(tit, result);
+    }
+
+
+    @Test
+    public void testgetDescripcion() {
+        Pelicula peli = new Pelicula(1, 1, "Esto es un título", "Esto es una descripción", "Esto es una sinopsis", 0, 0);
+        String des = "Esto es una descripción";
+        result = peli.getDescripcion();
+        assertEquals(des, result);
+    }
+
+
+    @Test
+    public void testsetDescripcion() {
+        Pelicula peli = new Pelicula(1, 1, "Esto es un título", "Esto es una descripción", "Esto es una sinopsis", 0, 0);
+        String des = "Descipcion descrita de forma describida";
+        peli.setDescripcion(des);
+        result = peli.getDescripcion();
+        assertEquals(des, result);
+    }
+}
