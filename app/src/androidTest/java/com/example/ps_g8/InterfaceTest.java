6f81f61cc8dc22cec7da7f3f1c57ac26bf84fde7
Santiago <santi.ari.pani@protonmail.com>
2023-04-26 14:50:26 +0200

    Tests interfaz completados

--- app/src/androidTest/java/com/example/ps_g8/InterfaceTest.java
+++ app/src/androidTest/java/com/example/ps_g8/InterfaceTest.java
@@ -1,33 +1,35 @@
 package com.example.ps_g8;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasBackground;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.drawable.Drawable;
-import android.os.Bundle;
 import android.widget.ImageButton;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -41,8 +43,11 @@ public class InterfaceTest {
             AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(context, "Administracion", null, 1);
             SQLiteDatabase db = admin.getWritableDatabase();
             admin.agregarPelis(db);
-            // Delete all users
+            // Prune database
             db.execSQL("DELETE FROM usuario;");
+            db.execSQL("DELETE FROM relacion;");
+            // Delete all movies except the first one
+            db.execSQL("DELETE FROM pelicula WHERE id != 1;");
             // Insert test user
             db.execSQL("INSERT INTO usuario (email, contraseña) VALUES ('user1@example.com', '1234');");
             db.execSQL("INSERT INTO relacion (id, usuario, gusta, visto) VALUES (1, 'user1@example.com', 0, 0);");
@@ -60,59 +65,49 @@ public class InterfaceTest {
 
     @Test
     public void testMeGustaButtonChangesIcon() {
-        // Seleccionar la referencia del botón
-        ActivityScenario<MainActivity2> btn = activityRule.getScenario().onActivity(activity -> {
-            Resources res = InstrumentationRegistry.getInstrumentation().getTargetContext().getResources();
-            int expectedIcon = res.getIdentifier("ic_baseline_favorite_border_24", "drawable", "com.example.ps_g8");
-            Drawable expectedDrawable = res.getDrawable(expectedIcon);
-            // Comprobar que el botón tiene el icono correcto (sin rellenar) al principio
-            ImageButton btn1 = activity.findViewById(R.id.imageButton1);
-            assertEquals(expectedDrawable, btn1.getDrawable());
-            // Hacer click en el botón
-            onView(withId(R.id.imageButton1)).perform(click());
-            // Comprobar que el botón tiene el icono correcto (rellenado) al final
-            expectedIcon = res.getIdentifier("ic_baseline_favorite_24", "drawable", "com.example.ps_g8");
-            expectedDrawable = res.getDrawable(expectedIcon);
-            assertEquals(expectedDrawable, btn1.getDrawable());
-            // Hacer click en el botón otra vez
-            onView(withId(R.id.imageButton1)).perform(click());
-            // Comprobar que el botón tiene el icono correcto (sin rellenar) al final
-            expectedIcon = res.getIdentifier("ic_baseline_favorite_border_24", "drawable", "com.example.ps_g8");
-            expectedDrawable = res.getDrawable(expectedIcon);
-            assertEquals(expectedDrawable, btn1.getDrawable());
+        // Buscar el botón de "me gusta" por su ID
+        onView(withId(R.id.imageButton1))
+                // Comprobar que el botón se muestra en pantalla
+                .check(matches(isDisplayed()));
+        int oldIconId = R.drawable.ic_baseline_favorite_border_24;
+        Drawable oldIcon = InstrumentationRegistry.getInstrumentation().getTargetContext().getDrawable(oldIconId);
+        onView(withId(R.id.imageButton1))
+                // Hacer clic en el botón
+                .perform(click());
+        // Obtener el icono del botón
+        activityRule.getScenario().onActivity(activity -> {
+            ImageButton button = activity.findViewById(R.id.imageButton1);
+            Drawable icon = button.getBackground();
+            // Comprobar que el icono cambia
+            assertNotEquals(oldIcon, icon);
         });
     }
 
     @Test
     public void testVistoButtonChangesIcon() {
-        // Seleccionar la referencia del botón
-        ActivityScenario<MainActivity2> btn = activityRule.getScenario().onActivity(activity -> {
-            Resources res = InstrumentationRegistry.getInstrumentation().getTargetContext().getResources();
-            int expectedIcon = res.getIdentifier("ic_baseline_not_interested_eye_24", "drawable", "com.example.ps_g8");
-            Drawable expectedDrawable = res.getDrawable(expectedIcon);
-            // Comprobar que el botón tiene el icono correcto (no visto) al principio
-            ImageButton btn1 = activity.findViewById(R.id.imageButton2);
-            assertEquals(expectedDrawable, btn1.getDrawable());
-            // Hacer click en el botón
-            onView(withId(R.id.imageButton2)).perform(click());
-            // Comprobar que el botón tiene el icono correcto (visto) al final
-            expectedIcon = res.getIdentifier("ic_baseline_remove_red_eye_24", "drawable", "com.example.ps_g8");
-            expectedDrawable = res.getDrawable(expectedIcon);
-            assertEquals(expectedDrawable, btn1.getDrawable());
-            // Hacer click en el botón otra vez
-            onView(withId(R.id.imageButton2)).perform(click());
-            // Comprobar que el botón tiene el icono correcto (no visto) al final
-            expectedIcon = res.getIdentifier("ic_baseline_not_interested_eye_24", "drawable", "com.example.ps_g8");
-            expectedDrawable = res.getDrawable(expectedIcon);
-            assertEquals(expectedDrawable, btn1.getDrawable());
+        // Buscar el botón de "visto" por su ID
+        onView(withId(R.id.imageButton2))
+                // Comprobar que el botón se muestra en pantalla
+                .check(matches(isDisplayed()));
+        int oldIconId = R.drawable.ic_baseline_not_interested_24;
+        Drawable oldIcon = InstrumentationRegistry.getInstrumentation().getTargetContext().getDrawable(oldIconId);
+        onView(withId(R.id.imageButton2))
+                // Hacer clic en el botón
+                .perform(click());
+        // Obtener el icono del botón
+        activityRule.getScenario().onActivity(activity -> {
+            ImageButton button = activity.findViewById(R.id.imageButton2);
+            Drawable icon = button.getBackground();
+            // Comprobar que el icono cambia
+            assertNotEquals(oldIcon, icon);
         });
     }
 
     @Test
     public void testMenuDesplegable() {
         // Hacer click en el botón de opciones
-        onView(withId(R.menu.acordeon_opciones)).perform(click());
-        // Comprobar que se despliega el menú
-        onView(withId(R.menu.acordeon_opciones)).check(matches(isDisplayed()));
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        // Comprobar que se muestra el menú desplegable
+        onView(withText("Log Out")).check(matches(isDisplayed()));
     }
 }
